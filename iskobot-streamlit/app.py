d53b9ca271971a7090b1a70debb91632f207d39b
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-08-28 22:57:26 +0800

    chore: put streamlit application to streamlit folder

--- iskobot-streamlit/app.py
+++ iskobot-streamlit/app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 llm = Ollama(model="llama3.1:8b") #Specify model here
 
-st.title("ISKOBOT")
+st.title("Iskobot")
 
-prompt = st.text_area(label="Message ISKOBOT", label_visibility="hidden", placeholder="Message ISKOBOT")
+prompt = st.text_area(label="Message Iskobot", label_visibility="hidden", placeholder="Message Iskobot")
 
 if st.button("Ask"):
     if prompt:
-        with st.spinner("Generating response..."):
+        with st.spinner("Answering your question..."):
             st.write_stream(llm.stream(prompt, stop=['<|eot_id|>']))
